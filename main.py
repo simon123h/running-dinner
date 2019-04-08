4a026de55822ec4b8caf1ffd25c2449ae2681a7b
simon123h <simon123h@users.noreply.github.com>
2019-04-08 17:24:21 +0200

    Reenable random teams

--- main.py
+++ main.py
@@ -14,26 +14,26 @@ def main():
     from rudi import RunningDinner, Team, Person
     rudi = RunningDinner()
 
-    # load the teams from a file
-    import csv
-    with open("in.tsv") as csv_file:
-        csv_reader = csv.reader(csv_file, delimiter='\t')
-        for row in csv_reader:
-            if row[4] != "":
-                p = Person(row[1])
-                p.email = row[2]
-                t = Team()
-                t.addMember(p)
-                # try to geocode adress and add team to event if successfull
-                if t.setAdress(row[4] + ", Muenster"):
-                    rudi.addTeam(t)
+    # # load the teams from a file
+    # import csv
+    # with open("in.tsv") as csv_file:
+    #     csv_reader = csv.reader(csv_file, delimiter='\t')
+    #     for row in csv_reader:
+    #         if row[4] != "":
+    #             p = Person(row[1])
+    #             p.email = row[2]
+    #             t = Team()
+    #             t.addMember(p)
+    #             # try to geocode adress and add team to event if successfull
+    #             if t.setAdress(row[4] + ", Muenster"):
+    #                 rudi.addTeam(t)
 
-    # # create some random teams
-    # from numpy.random import normal
-    # for _ in range(81):
-    #     t = Team()
-    #     t.coords = (normal(), normal())
-    #     rudi.addTeam(t)
+    # create some random teams
+    from numpy.random import normal
+    for _ in range(81):
+        t = Team()
+        t.coords = (normal(), normal())
+        rudi.addTeam(t)
 
     # find optimal routes
     rudi.organize()
